f52d4e7d474fcbbf12a34cfb6dc955cbb55f6f31
Hem92j <rauljihemraj@gmail.com>
2024-06-17 10:50:16 +0200

    added urdf from git package panda_ign_moveit2

--- vbc_bringup/launch/vbc_gazebo.launch.py
+++ vbc_bringup/launch/vbc_gazebo.launch.py
@@ -17,13 +17,13 @@ def generate_launch_description():
     world = LaunchConfiguration('world')
 
     world_launch_arg = DeclareLaunchArgument(
-        "world", default_value='universal.sdf', description="name of the world file")
+        "world", default_value='panda_world.sdf', description="name of the world file")
 
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
         launch_arguments={'gz_args': world}.items(),
-        
+
     )
 
     return LaunchDescription([
